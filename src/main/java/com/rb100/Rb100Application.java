e345226d18576e27eb700df076248b9f2d8c4135
Matt Cadorette <mcadorette@gmail.com>
2018-12-04 13:21:33 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/rb100/Rb100Application.java
@@ -0,0 +1,12 @@
+package com.rb100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rb100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rb100Application.class, args);
+	}
+}
---
+++ src/test/java/com/rb100/Rb100ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rb100;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Rb100ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
